eb04d94f5ecd9a8fda90017be5404a6620a58752
AthenaBurness <athenaburness@gmail.com>
2023-12-10 20:00:10 +0000

    My answer to the Greggs Challenge, User Story 1 and 2, including Tests.

--- Greggs.Products.Api/Models/Product.cs
+++ Greggs.Products.Api/Models/Product.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace Greggs.Products.Api.Models;
 
 public class Product
 {
     public string Name { get; set; }
     public decimal PriceInPounds { get; set; }
+    public decimal PriceInEuros
+    {
+        get { return Math.Floor(PriceInPounds * 1.11m * 100) / 100; }
+    }
 }
--- Greggs.Products.UnitTests/UnitTest1.cs
+++ Greggs.Products.UnitTests/UnitTest1.cs
@@ -1,4 +1,6 @@
+using Greggs.Products.Api.Controllers;
 using System;
+using System.Linq;
 using Xunit;
 
 namespace Greggs.Products.UnitTests;
@@ -9,5 +11,28 @@ public class UnitTest1
     public void Test1()
     {
         throw new NotImplementedException("We have no tests :-(");
+    }
+
+    [Fact] 
+    public void UserStory1Test()
+    {
+        //Arange - design pattern for test writing
+        var products = new ProductController();
+        //Act 
+        var result = products.Get(0, 5).ToList();
+        //Assert
+        Assert.Equal(5, result.Count);
+    }
+
+    [Fact] 
+    public void UserStory2Test() {
+        //Arrange
+        var products = new ProductController();
+        //Act
+        var product1 = products.Get(0, 1).ToList();
+        var product2 = products.Get(1, 2).ToList();
+        //Assert
+        Assert.Equal(product1.First().PriceInPounds * 1.11m, product1.First().PriceInEuros);
+        Assert.Equal(product2.First().PriceInPounds * 1.11m, product2.First().PriceInEuros);
     }
 }
